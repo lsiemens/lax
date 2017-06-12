34c4714748d4a46f7169463d1bd392bb0db5b6b4
Luke Siemens <lsiemens@users.noreply.github.com>
2017-06-11 17:10:00 -0700

    lax.py has been rewritten

--- lax.py
+++ lax.py
@@ -38,19 +38,21 @@ class LaxPair:
         
         if tout is None:
             tout = 0
+
                 
         #LAX
 #        self.commutator_t = operators.commutator(operators.partial(self.t), self.L)
 #        self.commutator_A = operators.commutator(self.L, self.A)
         # partial_t^2 \psi = A\psi
-        self.commutator_t = operators.commutator(operators.partial(self.t)(operators.partial(self.x)), self.L)
-        self.commutator_A = operators.commutator(self.L, self.A)
+#        self.commutator_t = operators.commutator(operators.partial(self.t)(operators.partial(self.x)), self.L)
+#        self.commutator_A = operators.commutator(self.L, self.A)
         #LAX
 #        self.commutator_t = operators.commutator(operators.partial(self.t)(operators.partial(self.t)), self.L)
 #        self.commutator_A = operators.commutator(self.L, operators.add(operators.commutator(operators.partial(self.t), self.A), self.A(self.A)))
 
-        self.LAX = operators.add(self.commutator_t, self.commutator_A)
+#        self.LAX = operators.add(self.commutator_t, self.commutator_A)
         #only comment temporaily ----- READ THIS ------
+        self.LAX = operators.commutator(self.L, self.A)
         try:
             self.f = functions.function("f", 2)(self.x, self.t)        
         except ValueError:
@@ -81,17 +83,16 @@ class LaxPair:
             solutions = [solution for solution in solutions if all([len(value.free_symbols & set(variables)) == 0 for key, value in solution.items()])]
             
             if not (all([self._simplify(condition.subs(solution)) == 0 for condition in compatibility for solution in solutions]) and len(solutions) != 0):
-                print(compatibility)
-                print([self._simplify(condition.subs(solution)) for condition in compatibility for solution in solutions])
                 raise LaxError("One or more compatibility conditions failed.")
             
-            PDE = [equation for equation in [self._simplify(PDE.subs(solution)) for solution in solutions] if equation != 0]
+            #PDE = [(self.L, self.A, equation) for equation in [self._simplify(PDE.subs(solution)) for solution in solutions] if equation != 0]
+            PDE = [(self._simplify(self.L(self.f).to_sympy().subs(solution)), self._simplify(self.A(self.f).to_sympy().subs(solution)), equation) for (equation, solution) in [(self._simplify(PDE.subs(solution)), solution) for solution in solutions] if equation != 0]
             if len(PDE) == 0:
                 raise LaxError("All compatible PDEs are trivially zero.")
             
         self.PDE = PDE
 
-        print("PDEs found: [\n" + ",\n\n".join([str(P) for P in self.PDE]) + "\n]")
+        print("PDEs found: [\n" + ",\n\n".join([str(P[0]) + ";\n" + str(P[1]) + ";\n" + str(P[2]) for P in self.PDE]) + "\n]")
 
     def _simplify(self, expression):
         return sympy.simplify(sympy.expand(expression))
@@ -109,22 +110,55 @@ class GenerateLax:
         self.operators = {}
         self.L_operator_distribution = {}
         self.A_operator_distribution = {}
-        self.num_args_distribution = numpy.array([1, 1, 1, 1])
+        self.num_args_distribution = numpy.array([8, 4, 2, 1])
         
         self._reset()
 
     def findLaxPair(self):
         self._reset()
-
-        L = self._generateOperator(self.L_operator_distribution)
-        A = self._generateOperator(self.A_operator_distribution)
+        with timeout.timeout(self.tout):
+            L = self._generateOperator(self.L_operator_distribution)
+            A = self._generateOperator(self.A_operator_distribution)
         
-        return LaxPair(L, A, self.constants, self.x, self.t, self.tout)
+        try:
+            laxpair = LaxPair(L, A, self.constants, self.x, self.t, self.tout)
+        except NotImplementedError:
+            #still need to solve "no valid subset found" error
+            raise LaxError("just a bandaid")
+        return laxpair
+
+    def findPairs(self):
+        while True:
+            try:
+                self.findLaxPair()
+            except (LaxError, TimeoutError, KeyError):
+                continue
 
     def _generateOperator(self, distribution):
         #use sself.operators and distribution
-        operator = None
-        return operator
+        keys = list(self.operators.keys())
+        operator_weights = [distribution[key] for key in keys]
+        key_index = numpy.random.choice(len(keys), p=operator_weights/numpy.sum(operator_weights))
+        operator = self.operators[keys[key_index]]
+        
+        try:
+            operand_distribution = self.num_args_distribution[operator["min_args"]:operator["max_args"] + 1]
+        except TypeError:
+            operand_distribution = self.num_args_distribution[operator["min_args"]:operator["max_args"]]
+        operand_options = range(operator["min_args"], operator["min_args"] + len(operand_distribution))
+        num_operands = numpy.random.choice(operand_options, p=operand_distribution/numpy.sum(operand_distribution))
+        
+        arguments = [self._generateOperator(distribution) for _ in range(num_operands)]
+        if len(arguments) > 0:
+            if keys[key_index] == "constant":
+                return self._new_constant()(*arguments)
+            else:
+                return operator["function"](*arguments)
+        else:
+            if keys[key_index] == "constant":
+                return self._new_constant()
+            else:
+                return operator["function"]
     
     def _new_constant(self):
         const = functions.constant("c" + str(len(self.constants) + 1))
@@ -138,27 +172,15 @@ class GenerateLax:
         self.t = functions.variable("t")
         self.u = functions.function("u", 2)(self.x, self.t)
 
-        self.operators = {"add":{"function":operators.add, "min_args":1, "max_args":None, "weight":1},
-                          "commutator":{"function":operators.commutator, "min_args":2, "max_args":2, "weight":1},
-                          "multiply_u":{"function":operators.multiply(self.u), "min_args":0, "max_args":1, "weight":1},
-                          "partial_x":{"function":operators.partal(self.x), "min_args":0, "max_args":1, "weight":1},
-                          "partial_t":{"function":operators.partial(self.t), "min_args":0, "max_args":1, "weight":1},
-                          "constant":{"function":self._new_constant(), "min_args":1, "max_args":1, "weight":1}}
+        self.operators = {"add":{"function":operators.add, "min_args":1, "max_args":None},
+                          "commutator":{"function":operators.commutator, "min_args":2, "max_args":2},
+                          "multiply_u":{"function":operators.multiply(self.u), "min_args":0, "max_args":1},
+                          "partial_x":{"function":operators.partial(self.x), "min_args":0, "max_args":1},
+                          "partial_t":{"function":operators.partial(self.t), "min_args":0, "max_args":1},
+                          "constant":{"function":self._new_constant, "min_args":1, "max_args":1}}
 
-        self.L_operator_distribution = {"add":1, "commutator":1, "multiply_u":1, "partial_x":1, "partial_t":1, "constant":1}
-        self.A_operator_distribution = {"add":1, "commutator":1, "multiply_u":1, "partial_x":1, "partial_t":1, "constant":1}
+        self.L_operator_distribution = {"add":1, "commutator":2, "multiply_u":4, "partial_x":5, "partial_t":0, "constant":0}
+        self.A_operator_distribution = {"add":1, "commutator":2, "multiply_u":4, "partial_x":3, "partial_t":10, "constant":6}
 
-L = add(Dx(Dx), operators.commutator(Dx, operators.commutator(Dx, M(u))))
-
-A = add(
-        new_constant()(operators.commutator(Dx, M(u))(Dt)),
-        new_constant()(operators.commutator(Dt, M(u))(Dx)),
-        new_constant()(operators.commutator(Dx, M(u))(Dx)),
-#        new_constant()(operators.commutator(Dt, M(u))(Dt)),
-        new_constant()(operators.commutator(Dx(Dx), M(u))),
-        new_constant()(operators.commutator(Dx(Dt), M(u))),
-#        new_constant()(operators.commutator(Dt(Dt), M(u))),
-#        new_constant()(operators.commutator(Dt, M(u))(Dx))
-        )
-
-KdV = LaxPair(L, A, constants, x, t, 120)
+generator = GenerateLax(60)
+generator.findPairs()
