ce3ff58a4a53c07ede871439b3a9483dbccec3c0
Luke Siemens <lsiemens@users.noreply.github.com>
2017-07-08 16:34:31 -0700

    rewriting NPDE_solve.py

--- tools/NPDE_solve.py
+++ tools/NPDE_solve.py
@@ -1,83 +1,94 @@
-import numpy
 from matplotlib import pyplot
+import numpy
 
-class solution:
-    def __init__(self, xres, dx, yres, dy):
+class solver:
+    def __init__(self, x0, x1, xres, y0, y1, yres):
+        self.x0 = x0
+        self.x1 = x1
         self.xres = xres
-        self.dx = dx
+        self.y0 = y0
+        self.y1 = y1        
         self.yres = yres
-        self.dy = dy
-        
-        self.x0 = -xres*dx/2.0
-        self.x1 = xres*dx/2.0
-        self.y0 = -self.yres*self.dy/2.0
-        self.y1 = self.yres*self.dy/2.0
         
         #format self.data[x][y]
-        self.x = numpy.linspace(self.x0, self.x1, self.xres, False)
-        self.y = numpy.linspace(self.y0, self.y1, self.yres, False)
-        self.data = numpy.zeros((xres, yres))
+        self.x = numpy.linspace(self.x0, self.x1, self.xres, True)
+        self.dx = (self.x1 - self.x0)/(self.xres - 1)
+        self.y = numpy.linspace(self.y0, self.y1, self.yres, True)
+        self.dy = (self.y1 - self.y0)/(self.yres - 1)
+        self.data = numpy.zeros((self.xres, self.yres))
         
-        self.U = profile(self.xres, self.dx, self.dy)
-        self.U_t = profile(self.xres, self.dx, self.dy)
-        
-    def solve(self, function_callback, initalization_callback):
+        #Assuming the folowing: self.U.shape == (n, self.xres) where n >= 1
+        self.U = None
+
+    def solve(self, initalization_callback, function_callback):
         initalization_callback(self)
-        #assuming self.U has now been initalized add it to self.data
-        self.data[:,0] = self.U.data
-        for i in range(self.yres - 1):
-#            pyplot.plot(self.x, self.U.data)
-#            pyplot.show()
-            self.U_t = function_callback(self)
-            self.U = self.U_t.integrate(self.U)
-            self.data[:, i] = self.U.data
-        pyplot.plot(self.x, self.data[:, 0])
-        pyplot.show()
-        pyplot.imshow(self.data, clim=(-2, 2))
-        pyplot.show()
-
-class profile:
-    def __init__(self, xres, dx, dy):
-        self.xres = xres
-        self.dx = dx
-        self.dy = dy
-        
-        self.data = numpy.zeros((xres,))
-
-    def derivative(self):
-        output = profile(self.xres, self.dx, self.dy)
-        output.data[1:-1] = ((self.data[:-2] - self.data[2:])/(2*self.dx))
-        output.data[0] = (self.data[0] - self.data[1])/self.dx
-        output.data[-1] = (self.data[-2] - self.data[-1])/self.dx
-        return output
+        self.data[:, 0] = self.U[0]
+        for i in range(1, self.yres - 1):
+            U_t = function_callback(self, self.U)
+            self.U = self.integrate_y(self.U, U_t)
+            self.data[:, i] = self.U[0]
     
-    def integrate(self, inital_profile):
-        output = profile(self.xres, self.dx, self.dy)
-        output.data = inital_profile.data + self.data*self.dy
+    def derivative(self, profile):
+        output = (numpy.roll(profile, 1) - numpy.roll(profile, -1))/(2*self.dx)
+#        output = (-numpy.roll(profile, 2) + 8*numpy.roll(profile, 1) - 8*numpy.roll(profile, -1) + numpy.roll(profile, -2))/(12*self.dx)
+        return output
+
+    def integrate_y(self, profile, derivative_y):
+        output = profile + derivative_y*self.dy
+        return output
+
+    def integrate_x(self, inital_value, function_callback):
+        output = numpy.zeros(shape=derivative_x.shape)
+        output[:, 0] = inital_value
+        for i in range(1, self.xres - 1):
+            output[:, i] = output[:, i - 1] + function_callback(self.U[:, i - 1], output[:, i - 1])*self.dx
         return output
 
 def init(self):
-#    self.U.data = numpy.exp(-self.x**2)
-#    self.U_t.data = -2*self.x*numpy.exp(-self.x**2)
-    self.U.data = numpy.cos(self.x*3) + numpy.cos(self.x*4)
-    self.U_t.data = numpy.sin(self.x*3) + numpy.sin(self.x*4)
+#    a = 2*numpy.pi/(self.x1 - self.x0)
+    self.U = numpy.zeros((1, self.xres))
+#    self.U = numpy.zeros((2, self.xres))
+    self.U[0] = numpy.exp(-self.x**2)
+#    self.U[1] = -2*self.x*numpy.exp(-self.x**2)
+#    self.U[0] = numpy.cos(4*a*self.x) + numpy.cos(7*a*self.x)       
+#    self.U[1] = 4*a*numpy.sin(4*a*self.x) + 7*a*numpy.sin(7*a*self.x)
+    self.guess = numpy.zeros((1, self.xres))
+    self.guess[0] = -2*self.x*numpy.exp(-self.x**2)
 
-def sdiv(x, dx=0.01):
-    return numpy.tanh(x/dx)/numpy.sqrt(dx**2 + x**2)
+def func(self, U):
+    output = numpy.zeros(shape=U.shape)
 
-def func(self):
-    U_t = profile(self.xres, self.dx, self.dy)
-#    U_t.data = self.U.data*self.U.derivative().data # u*u_0 + u_1
-#    U_t.data = self.U.derivative().data**2 - self.U.derivative().data # u*u_01 + u_0*u_1
-#    U_t.data = self.U.data**3*self.U.derivative().data*self.U_t.data + self.U.derivative().data # u*u_01 + u_0*u_1
-    U_tt = profile(self.xres, self.dx, self.dy)
-    U_tt.data = -self.U.data*self.U_t.derivative().data + self.U_t.data*self.U.derivative().data
-    U_t = U_tt.integrate(self.U_t)
-    return U_t
+    output[0] = U[0]*self.derivative(self.guess[0])
+    self.guess[0] = output[0]
+#    pyplot.plot(self.x, output[0])
+#    pyplot.show()
 
-#A = solution(2000, 0.005, 2000, 0.001)
-a = 2
-b = 1
-A = solution(2000*a, 0.005/a, 2000*a, 0.001/(a*b))
-A.solve(func, init)
+    output[0] = U[0]*self.derivative(self.guess[0])
+    self.guess[0] = output[0]
+#    pyplot.plot(self.x, output[0])
+#    pyplot.show()
 
+    output[0] = U[0]*self.derivative(self.guess[0])
+    self.guess[0] = output[0]
+#    pyplot.plot(self.x, output[0])
+#    pyplot.show()
+
+#    output[0] = U[0]*self.derivative(U[0])
+#    output[0] = self.derivative(U[0])**2 - self.derivative(U[0])
+    
+#    output[0] = U[1]
+#    output[1] = -U[0]*self.derivative(U[1]) + U[1]*self.derivative(U[0])
+
+#    output[0] = U[1]
+#    output[1] = self.derivative(self.derivative(U[0]))
+
+    
+    return output
+
+A = solver(-3, 3, 200, 0, 2, 200000)
+#A.solve(init, func)
+
+#pyplot.plot(A.x, A.data[:, 0])
+#pyplot.show()
+#pyplot.imshow(A.data[:,::1000], clim=(-0, 1.5))
+#pyplot.show()
