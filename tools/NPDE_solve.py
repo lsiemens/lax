3d2a01616cd911e71bc7f74b192562a75c832475
Luke Siemens <lsiemens@users.noreply.github.com>
2017-07-10 08:17:05 -0700

    Added integrate_x for integrating spatial derivative profiles

--- tools/NPDE_solve.py
+++ tools/NPDE_solve.py
@@ -23,56 +23,61 @@ class solver:
     def solve(self, initalization_callback, function_callback):
         initalization_callback(self)
         self.data[:, 0] = self.U[0]
-        for i in range(1, self.yres - 1):
+        for i in range(1, self.yres):
             U_t = function_callback(self, self.U)
             self.U = self.integrate_y(self.U, U_t)
             self.data[:, i] = self.U[0]
     
     def derivative(self, profile):
-        output = (numpy.roll(profile, 1) - numpy.roll(profile, -1))/(2*self.dx)
-#        output = (-numpy.roll(profile, 2) + 8*numpy.roll(profile, 1) - 8*numpy.roll(profile, -1) + numpy.roll(profile, -2))/(12*self.dx)
+        output = -(numpy.roll(profile, 1) - numpy.roll(profile, -1))/(2*self.dx)
+        output = -(-numpy.roll(profile, 2) + 8*numpy.roll(profile, 1) - 8*numpy.roll(profile, -1) + numpy.roll(profile, -2))/(12*self.dx)
         return output
 
     def integrate_y(self, profile, derivative_y):
         output = profile + derivative_y*self.dy
         return output
 
-    def integrate_x(self, inital_value, function_callback):
-        output = numpy.zeros(shape=derivative_x.shape)
+    def integrate_x(self, inital_value, U, function_callback, *args):
+        output = numpy.zeros((len(inital_value), self.xres))
         output[:, 0] = inital_value
-        for i in range(1, self.xres - 1):
-            output[:, i] = output[:, i - 1] + function_callback(self.U[:, i - 1], output[:, i - 1])*self.dx
+        for i in range(1, self.xres):
+            output[:, i] = output[:, i - 1] + function_callback(self, U[:, i - 1], output[:, i - 1], *[arg[:, i - 1] for arg in args])*self.dx
         return output
 
 def init(self):
-#    a = 2*numpy.pi/(self.x1 - self.x0)
-    self.U = numpy.zeros((1, self.xres))
+    a = 2*numpy.pi/(self.x1 - self.x0)
+    self.U = numpy.zeros((2, self.xres))
+    self.V = numpy.zeros((2, self.yres))
 #    self.U = numpy.zeros((2, self.xres))
     self.U[0] = numpy.exp(-self.x**2)
-#    self.U[1] = -2*self.x*numpy.exp(-self.x**2)
-#    self.U[0] = numpy.cos(4*a*self.x) + numpy.cos(7*a*self.x)       
+    self.V[0] = numpy.exp(-self.y**2)
+    self.U[1] = -2*self.x*numpy.exp(-self.x**2)
+    self.V[1] = numpy.exp(-self.y**2)
+#    self.U[0] = numpy.cos(4*a*self.x) + numpy.cos(7*a*self.x) + 2
 #    self.U[1] = 4*a*numpy.sin(4*a*self.x) + 7*a*numpy.sin(7*a*self.x)
-    self.guess = numpy.zeros((1, self.xres))
-    self.guess[0] = -2*self.x*numpy.exp(-self.x**2)
+#    self.V[0] = 4*a*numpy.sin(4*a*self.y) + 7*a*numpy.sin(7*a*self.y)
+#    self.V[1] = numpy.cos(4*a*self.y) + numpy.cos(7*a*self.y)
 
 def func(self, U):
     output = numpy.zeros(shape=U.shape)
+    V = self.V[:, 0]
+    self.V = self.V[:, 1:]
 
-    output[0] = U[0]*self.derivative(self.guess[0])
-    self.guess[0] = output[0]
-#    pyplot.plot(self.x, output[0])
+    U_x = self.derivative(U)
+    U_xx = self.derivative(U_x)
+    
+    def func2(self, U, V, U_x, U_xx):
+        output = numpy.zeros(shape=V.shape)
+        output[:-1] = V[1:]
+        output[-1] = (V[0] - U_xx[0] - U_x[0]*U[1])/(U[0]**2 + 10)
+        return output
+    
+    u_tm = self.integrate_x(V, U, func2, U_x, U_xx)[0, :]
+#    pyplot.plot(self.x, u_tm)
 #    pyplot.show()
-
-    output[0] = U[0]*self.derivative(self.guess[0])
-    self.guess[0] = output[0]
-#    pyplot.plot(self.x, output[0])
-#    pyplot.show()
-
-    output[0] = U[0]*self.derivative(self.guess[0])
-    self.guess[0] = output[0]
-#    pyplot.plot(self.x, output[0])
-#    pyplot.show()
-
+    output[:-1] = U[1:]
+    output[-1] = u_tm
+    
 #    output[0] = U[0]*self.derivative(U[0])
 #    output[0] = self.derivative(U[0])**2 - self.derivative(U[0])
     
@@ -81,14 +86,14 @@ def func(self, U):
 
 #    output[0] = U[1]
 #    output[1] = self.derivative(self.derivative(U[0]))
-
-    
     return output
 
-A = solver(-3, 3, 200, 0, 2, 200000)
-#A.solve(init, func)
+A = solver(-3, 3, 2000, -3, 3, 2000)
+A.solve(init, func)
 
-#pyplot.plot(A.x, A.data[:, 0])
-#pyplot.show()
-#pyplot.imshow(A.data[:,::1000], clim=(-0, 1.5))
-#pyplot.show()
+pyplot.plot(A.x, A.data[:, 0])
+pyplot.show()
+pyplot.imshow(A.data)
+pyplot.show()
+pyplot.imshow(numpy.log(numpy.abs(A.data)), clim=(0, None))
+pyplot.show()
